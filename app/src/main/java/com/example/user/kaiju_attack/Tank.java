0ecbad2558e98c9145d1e42547eef76767fd3985
Alistair Kane <vplelmo@hotmail.co.uk>
2017-10-25 22:25:16 +0100

    MVP met

--- app/src/main/java/com/example/user/kaiju_attack/Tank.java
+++ app/src/main/java/com/example/user/kaiju_attack/Tank.java
@@ -1,12 +1,14 @@
 package com.example.user.kaiju_attack;
 
+import com.example.user.kaiju_attack.behaviours.VehicleAttack;
+
 /**
  * Created by user on 25/10/2017.
  */
 
-public class Tank extends Vehicle {
+public class Tank extends Vehicle implements VehicleAttack{
 
-    public Tank(String type, int healthValue, int bigGunValue, int littleGunValue) {
-        super(type, healthValue, bigGunValue, littleGunValue);
+    public Tank(String type, int healthValue, int littleGunValue, int bigGunValue) {
+        super(type, healthValue, littleGunValue, bigGunValue);
     }
 }
--- app/src/main/java/com/example/user/kaiju_attack/behaviours/VehicleAttack.java
+++ app/src/main/java/com/example/user/kaiju_attack/behaviours/VehicleAttack.java
@@ -1,9 +1,13 @@
 package com.example.user.kaiju_attack.behaviours;
 
+import com.example.user.kaiju_attack.Kaiju;
+
 /**
  * Created by user on 25/10/2017.
  */
 
 public interface VehicleAttack {
-    
+
+    int littleGunValue;
+    int bigGunValue;
 }
